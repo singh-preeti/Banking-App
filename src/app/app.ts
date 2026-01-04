77261eb24ed240dd80a03e90882faf762881c169
singh-preeti <singhpreety217@gmail.com>
2026-01-04 16:32:25 +0530

    added frontend

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AccountCreateComponent } from './components/account-create/account-create';
+import { AccountActionsComponent } from './components/account-actions/account-actions';
+import { TransactionHistoryComponent } from './components/transaction-history/transaction-history';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AccountCreateComponent, AccountActionsComponent, TransactionHistoryComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
